81c24ae2aee034778448548342a177f1ea4aa55b
Steve Owens <steve.owens@bjss.com>
2018-02-15 13:48:44 +0000

    Latest Version

---
+++ IAC/CICD-IAAS/VA-CICD-IAC/jenkins_backup.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# This script will configure a daily backup at 1am of the Jenkins $JENKINS_HOME configuration and upload to a git repository.
+#
+
+Exit
---
+++ IAC/CICD-IAAS/VA-CICD-IAC/jenkins_restore.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# This script will restore the Jenkins $JENKINS_HOME configuration from a git repository.
+#
+
+Exit
