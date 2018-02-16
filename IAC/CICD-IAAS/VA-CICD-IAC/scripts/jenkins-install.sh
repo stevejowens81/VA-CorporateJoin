613d9a6446812fb1b5c167640222537f5fb74c64
Steve Owens <steve.owens@bjss.com>
2018-02-16 12:11:22 +0000

    Update

--- IAC/CICD-IAAS/VA-CICD-IAC/scripts/jenkins-install.sh
+++ IAC/CICD-IAAS/VA-CICD-IAC/scripts/jenkins-install.sh
@@ -7,7 +7,7 @@
 #     Filename:         jenkins-install.sh
 #     This script install Jenkins in your Ubuntu System
 #    ===========================================================================
-wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
+wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-sudo apt-get -y update 
+sudo apt-get -y update
 sudo apt-get -y install jenkins
